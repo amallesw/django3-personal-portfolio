b66c05cbf653abcdd4acd53e4372b4aadb01cf00
Arjun Malleswaran <ajmallesh@Arjuns-MacBook-Pro-2.local>
2022-01-26 11:12:36 -0800

    first commit

---
+++ portfolio/models.py
@@ -0,0 +1,13 @@
+from distutils.command.upload import upload
+from django.db import models
+from matplotlib.pyplot import title
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    # not required
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
